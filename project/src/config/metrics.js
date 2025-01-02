a847c288eac1047f34d37446346130378fec1e21
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2025-01-02 12:22:59 +0200

    Delete project directory

--- project/src/config/metrics.js
+++
@@ -1,3 +0,0 @@
-// This file is now deprecated in favor of the modular structure
-// Keeping it as a re-export for backward compatibility
-module.exports = require('./metrics/index');
--- project/src/config/metrics/registry.js
+++
@@ -1,6 +0,0 @@
-const promClient = require('prom-client');
-
-// Create and export a single registry instance
-const register = new promClient.Registry();
-
-module.exports = register;
